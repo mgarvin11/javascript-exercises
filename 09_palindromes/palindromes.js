83b56c910f6160276ba4fe9e503245d849db545b
mgarvin11 <mgarvin11@gmail.com>
2024-07-18 20:39:34 -0400

    8-12 coded

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
 const palindromes = function () {
 
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanedString = string
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+
+    const reversedString = cleanedString.split('').reversed().join('');
+
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
